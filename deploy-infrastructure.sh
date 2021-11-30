9e6b437d20784c9d31483a688da19cdd73087610
Nicolas Bock <nicolas.bock@canonical.com>
2021-11-30 09:33:15 -0700

    More infrastructure script
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- deploy-infrastructure.sh
+++ deploy-infrastructure.sh
@@ -2,31 +2,54 @@
 
 set -x
 
+# Storage nodes
 for i in {1..3}; do
   ./add-machine-hypervisor.sh \
-    --name juju-controller-${i} \
-    --memory $((2 * 1024)) \
-    --tag juju \
-    --force \
-    --bus sata \
-    --network maas-admin-net \
-    --network maas-internal-net \
-    --network maas-external-net \
-    --network maas-public-net
-done
-
-for i in {1..6}; do
-  ./add-machine-hypervisor.sh \
-    --name infra-1 \
+    --name infra-$(printf "%02d" ${i}) \
     --debug \
-    --memory $((4 * 1024)) \
+    --memory $((2 * 1024)) \
     --force \
     --tag infra \
     --tag storage \
+    --tag small \
     --bus sata \
-    --disk 50 \
+    --disk 20 \
     --disk 10 \
     --disk 10 \
+    --network maas-admin-net \
+    --network maas-internal-net \
+    --network maas-external-net
+done
+
+# Small computes
+for i in {4..8}; do
+  ./add-machine-hypervisor.sh \
+    --name infra-$(printf "%02d" ${i}) \
+    --debug \
+    --memory $((2 * 1024)) \
+    --force \
+    --tag infra \
+    --tag compute \
+    --tag small \
+    --bus sata \
+    --disk 20 \
+    --network maas-admin-net \
+    --network maas-internal-net \
+    --network maas-external-net
+done
+
+# Large computes
+for i in {9..12}; do
+  ./add-machine-hypervisor.sh \
+    --name infra-$(printf "%02d" ${i}) \
+    --debug \
+    --memory $((8 * 1024)) \
+    --force \
+    --tag infra \
+    --tag compute \
+    --tag large \
+    --bus sata \
+    --disk 40 \
     --network maas-admin-net \
     --network maas-internal-net \
     --network maas-external-net
