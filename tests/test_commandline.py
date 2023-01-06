54eebe3b278a3c7b212aaee5c370382c0b3ff13b
Nicolas Bock <nicolas.bock@canonical.com>
2023-01-06 10:36:53 -0700

    Add `--series` argument
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -6,6 +6,7 @@ try:
     from unittest.mock import patch
 except ImportError:
     from mock import patch  # type: ignore
+import argparse
 import sys
 import contextlib
 import io
@@ -28,3 +29,29 @@ class TestCommandLine(unittest.TestCase):
             output_string = output.getvalue().rstrip()
             self.assertEqual(output_string, '0.0.0')
         self.assertIsNone(options)
+
+    def test_options(self):
+        """Test whether a Namespace object is returned."""
+        with patch.object(sys, 'argv', ['prog']):
+            options = maaslab.main.parse_commandline()
+        self.assertTrue(isinstance(options, argparse.Namespace))
+
+    def test_incorrect_series(self):
+        """Test `--series` argument with incorrect value."""
+        with patch.object(sys, 'argv', ['prog', '--series', 'xx']):
+            with self.assertRaises(SystemExit) as exit_code:
+                maaslab.main.parse_commandline()
+        self.assertEqual(exit_code.exception.code, 2)
+
+    def test_series(self):
+        """Test legal values of `--series` argument."""
+        for series in ['bionic', 'focal', 'jammy']:
+            with patch.object(sys, 'argv', ['prog', '--series', series]):
+                options = maaslab.main.parse_commandline()
+            self.assertEqual(options.series, series)
+
+    def test_default_series(self):
+        """Test the default series."""
+        with patch.object(sys, 'argv', ['prog']):
+            options = maaslab.main.parse_commandline()
+        self.assertEqual(options.series, 'focal')
